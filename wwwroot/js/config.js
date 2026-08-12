d986958a7a1889c24655981cd478f4ed2b6580ea
Mario Medhat <118922155+Mariomedhat899@users.noreply.github.com>
2026-08-12 21:59:48 +0300

    Update config.js for separate API URL

--- wwwroot/js/config.js
+++ wwwroot/js/config.js
@@ -1,5 +1,7 @@
 window.IMS_API = {
-  BASE_URL: './api',
+  BASE_URL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+    ? './api'
+    : 'https://<YOUR-API-URL>',
   TOKEN_KEY: 'ims_token',
   EMAIL_KEY: 'ims_email',
   ROLE_KEY: 'ims_role'
