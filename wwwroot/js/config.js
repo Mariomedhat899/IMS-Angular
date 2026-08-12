c3245d54f5cbfc19fdd4f39908d40045bfd7e858
Mario Medhat <118922155+Mariomedhat899@users.noreply.github.com>
2026-08-12 22:49:26 +0300

    Point frontend to http://imsapp.runasp.net API

--- wwwroot/js/config.js
+++ wwwroot/js/config.js
@@ -1,7 +1,7 @@
 window.IMS_API = {
   BASE_URL: (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
     ? './api'
-    : 'https://<YOUR-API-URL>',
+    : 'http://imsapp.runasp.net',
   TOKEN_KEY: 'ims_token',
   EMAIL_KEY: 'ims_email',
   ROLE_KEY: 'ims_role'
